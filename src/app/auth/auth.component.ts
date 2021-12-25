aa313bc86c2f6b41a48b5e255110ae957063ffe8
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-12-25 22:08:14 +0100

    template form implemented with validations

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+
+  loginOrSignUp = true;
+
+  switchMode() {
+    this.loginOrSignUp = !this.loginOrSignUp;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+}
